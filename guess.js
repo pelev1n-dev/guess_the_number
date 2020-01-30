0f7d7c0e704f7072e1639bb40fa2da6aef7820c4
Pelevin Aleksandr <web.it.seb@gmail.com>
2020-01-30 18:53:57 +0300

    light version game

---
+++ guess.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let number = Math.floor(Math.random() * 100);
+
+function guess() {
+  let userAnswer = +prompt('Введите число от 0 до 100');
+
+  if(typeof (userAnswer) !== 'number') {
+    userAnswer = +prompt('Введите число от 0 до 100');
+  }
+
+  if (userAnswer > number) {
+    alert('Загаданное число меньше');
+    guess();
+  }
+
+  if (userAnswer < number) {
+    alert('Загаданное число больше');
+    guess();
+  }
+
+  if (userAnswer === number){
+    confirm('Вы угадали! Продолжить или завершить игру?')
+  }
+
+}
+
+guess();
+
+
+
+
+
+
+
+
+
+
