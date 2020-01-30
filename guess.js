911bc8c224c3f4d6d2b181ad1ca3eef9bd539399
Pelevin Aleksandr <web.it.seb@gmail.com>
2020-01-30 19:09:43 +0300

    easy version done

--- guess.js
+++ guess.js
@@ -5,11 +5,6 @@ let number = Math.floor(Math.random() * 100);
 function guess() {
   let userAnswer = +prompt('Введите число от 0 до 100');
 
-  if(typeof (userAnswer) !== 'number') {
-    userAnswer = +prompt('Введите число от 0 до 100');
-    guess();
-  }
-
   if (userAnswer > number) {
     alert('Загаданное число меньше');
     guess();
