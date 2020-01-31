c773acb769f47140a17f595978784ad3e44cc1b9
Pelevin Aleksandr <web.it.seb@gmail.com>
2020-01-31 10:25:40 +0300

    edit guess.js

--- guess.js
+++ guess.js
@@ -6,42 +6,41 @@ const guessTheNumber = (function () {
     return Math.floor(Math.random() * 100);
   }
 
-  function checkCounter() {
-    counter--;
-    if (counter === 0) {
-      if (confirm(`Игра завершена у вас ${counter} попыток. Начать новую игру?`)){
-        return guessTheNumber();
-      }
-    }
-  }
-
   let counter = 10;
   let number = getRandomNumber();
 
 
   return function guess() {
 
-    let userAnswer = +prompt(`Введите число от 1 до 100. У вас ${counter} попыток`);
-    checkCounter();
+    if (counter === 0) {
+      if (confirm(`Игра завершена у вас ${counter} попыток. Начать новую игру?`)){
+        counter = 10;
+        getRandomNumber();
+        guessTheNumber();
+      }
+      return
+    }
 
-    if(isNaN(userAnswer)){
+    let userAnswer = +prompt(`Введите число от 1 до 100.`);
+
+    if(null === userAnswer){
+      return
+    } else if(isNaN(userAnswer)){
       alert('Введи число!');
       guess();
-    }
-
-    if (userAnswer > number) {
+    } else if(userAnswer > number){
+      counter--;
       alert(`Загаданное число меньше. Осталось попыток ${counter}`);
       guess();
-    }
-
-    if (userAnswer < number) {
+    } else if(userAnswer < number){
+      counter--;
       alert(`Загаданное число больше. Осталось попыток ${counter}`);
       guess();
-    }
-
-    if (userAnswer === number){
+    } else if(userAnswer === number) {
       if(confirm("Вы угадали! Продолжить или завершить игру?")){
-        return guessTheNumber();
+        counter = 10;
+        getRandomNumber();
+        guessTheNumber();
       }
     }
 
@@ -51,60 +50,5 @@ const guessTheNumber = (function () {
 
 guessTheNumber();
 
-/*function guessTheNumber() {
 
-  function getRandomNumber() {
-    return Math.floor(Math.random() * 100);
-  }
-  let number = getRandomNumber();
-
-  let counter = 10;
-  function checkCounter() {
-    counter--;
-    if (counter === 0) {
-      endGame();
-    } else {
-      guess();
-    }
-  }
-
-  function guess() {
-
-    let userAnswer = +prompt('Введите число от 1 до 100');
-
-    if(isNaN(userAnswer)){
-      alert('Введи число!');
-      guess();
-    }
-
-    if (userAnswer > number) {
-      alert(`Загаданное число меньше. Осталось попыток ${counter}`);
-      checkCounter();
-      guess();
-    }
-
-    if (userAnswer < number) {
-      alert(`Загаданное число больше. Осталось попыток ${counter}`);
-      checkCounter();
-      guess();
-    }
-
-    if (userAnswer === number){
-      if (confirm("Вы угадали! Продолжить или завершить игру?")) {
-        guessTheNumber();
-      } else {
-        endGame();
-      }
-    }
-
-  }
-
-  function endGame() {
-    alert('Игра завершена');
-    guessTheNumber();
-  }
-
-  return guess();
-
-}*/
 
